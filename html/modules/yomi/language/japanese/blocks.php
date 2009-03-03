0de0d1f7be21362cf622624e80d944c6c00af459
nao-pon <hypweb@gmail.com>
2009-03-03 23:58:25 +0000

    Include 'main.php'.

--- html/modules/yomi/language/japanese/blocks.php
+++ html/modules/yomi/language/japanese/blocks.php
@@ -5,5 +5,5 @@ define("_MB_YOMI_NEWREG","登録する");
 define("_MB_YOMI_NEWARV","新着サイト");
 define("_MB_YOMI_NUM","表示する件数:");
 define("_MB_YOMI_PER","集計期間(日数):");
-
+include_once(dirname(__FILE__) . '/main.php');
 ?>
