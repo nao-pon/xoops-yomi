e140b4a3aa0f5796684ff2f9c8ebe62801481be1
nao-pon <hypweb@gmail.com>
2010-04-19 01:29:16 +0000

    Bugfix, Problem of "Fatal Error" on PHP 4.

--- html/modules/yomi/init.php
+++ html/modules/yomi/init.php
@@ -30,7 +30,7 @@ $Efld=16;  #0¡Á15¤Þ¤Ç
 
 $Eend="";
 
-// Setup $EST 
+// Setup $EST
 if (! defined('_MD_YOMI_INIT_LOADED')) {
 	define('_MD_YOMI_INIT_LOADED', TRUE);
 	$EST['script'] = $EST['cgi_path_url'] . basename($EST['script']);
@@ -48,4 +48,43 @@ if (empty($EST['shorturl']))
 	$Ekt = $EST['script'] . "?mode=kt&kt=";
 else
 	$Ekt = XOOPS_URL."/".$EST['shorturl']."/";
-?>
+
+// For PHP 4
+if (! function_exists('array_combine')) {
+function array_combine($keys, $values)
+{
+	if (!is_array($keys)) {
+		user_error('array_combine() expects parameter 1 to be array, ' .
+			gettype($keys) . ' given', E_USER_WARNING);
+		return;
+	}
+
+	if (!is_array($values)) {
+		user_error('array_combine() expects parameter 2 to be array, ' .
+			gettype($values) . ' given', E_USER_WARNING);
+		return;
+	}
+
+	$key_count = count($keys);
+	$value_count = count($values);
+	if ($key_count !== $value_count) {
+		user_error('array_combine() Both parameters should have equal number of elements', E_USER_WARNING);
+		return false;
+	}
+
+	if ($key_count === 0 || $value_count === 0) {
+		user_error('array_combine() Both parameters should have number of elements at least 0', E_USER_WARNING);
+		return false;
+	}
+
+	$keys	 = array_values($keys);
+	$values	 = array_values($values);
+
+	$combined = array();
+	for ($i = 0; $i < $key_count; $i++) {
+		$combined[$keys[$i]] = $values[$i];
+	}
+
+	return $combined;
+}
+}
