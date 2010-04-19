eca0b718724fff5d0ef34105569b22ba5b500bb0
nao-pon <hypweb@gmail.com>
2010-04-19 01:36:58 +0000

    Moved compat function from init.php to include/func.inc.php.

--- html/modules/yomi/include/func.inc.php
+++ html/modules/yomi/include/func.inc.php
@@ -2,7 +2,7 @@
 /*
  * Created on 2008/06/23 by nao-pon http://hypweb.net/
  * License: GPL v2 or (at your option) any later version
- * $Id: func.inc.php,v 1.3 2010/03/11 00:34:32 nao-pon Exp $
+ * $Id: func.inc.php,v 1.4 2010/04/19 01:36:58 nao-pon Exp $
  */
 
 if( ! class_exists( 'HypCommonFunc' ) ) {
@@ -19,7 +19,7 @@ function yomi_get_banner_cache ($url, $id)
 	$false = array('', FALSE);
 	// URLチェック
 	if (!preg_match("#^https?://.+\.(jpe?g|png|gif)$#i",$url)) return $false;
-	
+
 	// 画像ディレクトリ パーミッション 666
 	$dir = XOOPS_ROOT_PATH."/modules/yomi/blocks/logos/";
 	$imgurl = XOOPS_URL."/modules/yomi/blocks/logos/";
@@ -30,26 +30,26 @@ function yomi_get_banner_cache ($url, $id)
 	$imgurl = $imgurl.$id.$ext;
 	if (file_exists($filename) && filemtime($filename) + $cache_h*3600 > time())
 		return array($imgurl, getimagesize($filename));
-	
+
 	// 指定ファイルをキャッシュする
-	
+
 	$d = new Hyp_HTTP_Request();
 	$d->url = $url;
 	$d->connect_timeout = 3;
 	$d->read_timeout = 2;
 	$d->get();
-	
+
 	if ($d->rc !== 200 || !$d->data) {
 		$d->data = join('', file(XOOPS_ROOT_PATH."/modules/yomi/img/noimage.gif"));
 	}
-	
+
 	if($fp = fopen($filename, "wb")) {
 		fwrite($fp, $d->data);
 		fclose($fp);
 	}
-	
+
 	$imagesize = getimagesize($filename);
-	
+
 	if (!isset($imagesize[2]) || $imagesize[2] < 1 || $imagesize[2] > 3) {
 		$d->data = join('', file(XOOPS_ROOT_PATH."/modules/yomi/img/noimage.gif"));
 		if($fp = fopen($filename, "wb")) {
@@ -59,7 +59,7 @@ function yomi_get_banner_cache ($url, $id)
 		clearstatcache();
 		$imagesize = getimagesize($filename);
 	}
-	
+
 	return array($imgurl, getimagesize($filename));
 }
 
@@ -75,4 +75,43 @@ function yomi_get_favicon($url, $alt = '') {
 	}
 	return '<img src="' . XOOPS_URL . '/class/hyp_common/favicon.php?url=' . rawurlencode($url) . '" width="16" height="16" alt="'.$alt.'" />';
 }
-?>
+
+// For PHP 4
+if (! function_exists('array_combine')) {
+function array_combine($keys, $values)
+{
+	if (!is_array($keys)) {
+		user_error('array_combine() expects parameter 1 to be array, ' .
+			gettype($keys) . ' given', E_USER_WARNING);
+		return;
+	}
+
+	if (!is_array($values)) {
+		user_error('array_combine() expects parameter 2 to be array, ' .
+			gettype($values) . ' given', E_USER_WARNING);
+		return;
+	}
+
+	$key_count = count($keys);
+	$value_count = count($values);
+	if ($key_count !== $value_count) {
+		user_error('array_combine() Both parameters should have equal number of elements', E_USER_WARNING);
+		return false;
+	}
+
+	if ($key_count === 0 || $value_count === 0) {
+		user_error('array_combine() Both parameters should have number of elements at least 0', E_USER_WARNING);
+		return false;
+	}
+
+	$keys	 = array_values($keys);
+	$values	 = array_values($values);
+
+	$combined = array();
+	for ($i = 0; $i < $key_count; $i++) {
+		$combined[$keys[$i]] = $values[$i];
+	}
+
+	return $combined;
+}
+}
--- html/modules/yomi/init.php
+++ html/modules/yomi/init.php
@@ -6,12 +6,13 @@
 
 global $ganes, $gane_top, $EST;
 
-require_once dirname(__FILE__).'/pl/cfg.php';
-require_once dirname(__FILE__).'/pl/temp.php';
-include_once XOOPS_ROOT_PATH . '/modules/yomi/include/func.inc.php';
+define('_MI_YOMI_ROOT_PATH', dirname(__FILE__));
+require_once _MI_YOMI_ROOT_PATH . '/pl/cfg.php';
+require_once _MI_YOMI_ROOT_PATH . '/pl/temp.php';
+include_once _MI_YOMI_ROOT_PATH . '/include/func.inc.php';
 
 #バージョン情報
-include dirname(__FILE__).'/version.php';
+include _MI_YOMI_ROOT_PATH . '/version.php';
 $ver = $_md_yomi_info['ver'];
 $x_ver = $_md_yomi_info['x_ver'];
 
@@ -48,43 +49,3 @@ if (empty($EST['shorturl']))
 	$Ekt = $EST['script'] . "?mode=kt&kt=";
 else
 	$Ekt = XOOPS_URL."/".$EST['shorturl']."/";
-
-// For PHP 4
-if (! function_exists('array_combine')) {
-function array_combine($keys, $values)
-{
-	if (!is_array($keys)) {
-		user_error('array_combine() expects parameter 1 to be array, ' .
-			gettype($keys) . ' given', E_USER_WARNING);
-		return;
-	}
-
-	if (!is_array($values)) {
-		user_error('array_combine() expects parameter 2 to be array, ' .
-			gettype($values) . ' given', E_USER_WARNING);
-		return;
-	}
-
-	$key_count = count($keys);
-	$value_count = count($values);
-	if ($key_count !== $value_count) {
-		user_error('array_combine() Both parameters should have equal number of elements', E_USER_WARNING);
-		return false;
-	}
-
-	if ($key_count === 0 || $value_count === 0) {
-		user_error('array_combine() Both parameters should have number of elements at least 0', E_USER_WARNING);
-		return false;
-	}
-
-	$keys	 = array_values($keys);
-	$values	 = array_values($values);
-
-	$combined = array();
-	for ($i = 0; $i < $key_count; $i++) {
-		$combined[$keys[$i]] = $values[$i];
-	}
-
-	return $combined;
-}
-}
